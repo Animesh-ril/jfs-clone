537fbd3ccfc13741a8b9b5a5f5f592b18100f5f6
nehallsharma <sharmanehal347@gmail.com>
2024-02-12 16:37:19 +0530

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,6 @@
-<<<<<<< HEAD
-
-// // pages/index.js
-// import Head from 'next/head'
-// import MultiLineCarousel from './components/MultilineCarousel';
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <Head>
-//         <title>Multi-line Carousel</title>
-//         <meta name="description" content="Multi-line Carousel Example" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main>
-//         <h1>Multi-line Carousel Example</h1>
-//         <MultiLineCarousel />
-//       </main>
-//     </div>
-//   )
-// }
-// pages/index.js
 import Head from 'next/head'
 import Card from './components/Card'
 import Footer from './components/Footer'
-=======
-import Image from "next/image";
-import Navbar from "./Components/Navbar";
-import "./globals.css"
->>>>>>> beab1f5f7a064960ae5657e667b9f99c634b989c
 
 export default function Home() {
   return (
