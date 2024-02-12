c30a5dbe962272d76a2dd049dc6d6895d7e2c9bc
esther24 <estherchettiar24@gmail.com>
2024-02-12 16:29:48 +0530

    changed folders

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,16 @@
-<<<<<<< HEAD
 
-// // pages/index.js
-// import Head from 'next/head'
-// import MultiLineCarousel from './components/MultilineCarousel';
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <Head>
-//         <title>Multi-line Carousel</title>
-//         <meta name="description" content="Multi-line Carousel Example" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main>
-//         <h1>Multi-line Carousel Example</h1>
-//         <MultiLineCarousel />
-//       </main>
-//     </div>
-//   )
-// }
-// pages/index.js
 import Head from 'next/head'
 import Card from './components/Card'
 import Footer from './components/Footer'
-=======
-import Image from "next/image";
-import Navbar from "./Components/Navbar";
-import "./globals.css"
->>>>>>> beab1f5f7a064960ae5657e667b9f99c634b989c
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>JFS | Home</title>
-        <meta name="description" content="Your description here" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
+
+      <Navbar/>
       <main>
-        <h1>Welcome to JFS</h1>
+        
         <Card />
       </main>
       <Footer/>
