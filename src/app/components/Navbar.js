9c621036767ca419580166e3d49e4efe6e05526f
nehallsharma <sharmanehal347@gmail.com>
2024-02-12 13:19:40 +0530

    ..

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,30 @@
+// components/Navbar.js
+
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <h1>navbar</h1>
+        {/* <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li> */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
