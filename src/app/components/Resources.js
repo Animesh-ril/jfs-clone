ab802229748630526b19c0cc4c0debe9306ea7e8
nehallsharma <sharmanehal347@gmail.com>
2024-02-14 12:24:00 +0530

    alignment updated

--- src/app/components/Resources.js
+++ src/app/components/Resources.js
@@ -24,21 +24,22 @@ export default function Resource() {
       <div className="items-center">
         <div className=" items-center ">
           <h1 className=" text-white text-3xl font-JioTypeBold text-center">Find resources related to investor relations here</h1>
-          <p className="text-white text-l mt-5 font-JioTypeMedium text-center">JFinancial documents,policies,investors' contacts, and more.</p>
+          <p className="text-white text-l mt-5 font-JioTypeMedium text-center">JFinancial documents,policies,investors contacts, and more.</p>
         </div>
       </div>
       <div className="flex gap-4 justify-center w-full">
-        <div className="w-1/2 h-[470px] py-5 items-center">
+        <div className="w-1/2 h-[470px] py-5 items-center relative">
           <div style={{ backgroundImage: `url(${slides[0].url})` }} className="w-full h-full bg-center bg-cover rounded-3xl relative">
-            <div className="text-left ml-10 mx-3 flex justify-center items-end">
-              <div className="mt-20 mb-15"><h3 className="text-white text-4xl font-JioTypeBold  ">
+            <div className="flex w-full justify-between absolute bottom-4 left-0 px-8">
+              <div className=" mb-15"><h3 className="text-white text-4xl font-JioTypeBold  ">
                 Financial Documents
               </h3>
-              <br></br>
-              <p className="text-white text-l text-left font-JioTypeBold">
-                Access quarterly and annual reports.
-                <button class=" px-10 py-4 mx-end ml-40 font-JioTypeBold text-black bg-yellow-500  hover:bg-yellow-700 rounded-full w-fit">Explore Now</button>
-              </p>
+                <p className="text-white text-l mt-4 mb-5 text-left font-JioTypeBold">
+                  Access quarterly and annual reports.
+                </p>
+              </div>
+              <div className=" flex items-center ">
+                <button className=" px-10 py-4 mx-end  font-JioTypeBold text-black bg-yellow-500  hover:bg-yellow-700 rounded-full w-fit">Explore Now</button>
               </div>
               {/* <div className="flex "> 
               <button class=" px-10 py-4 mx-end ml-20 font-JioTypeBold text-black bg-yellow-500  hover:bg-yellow-700 rounded-full w-fit">Explore Now</button>
