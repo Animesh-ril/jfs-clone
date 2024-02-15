5b6e3f42cff6be8b111a7152d9535e34abc14ab7
esther24 <estherchettiar24@gmail.com>
2024-02-15 11:47:57 +0530

    added scripts in page

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <nav>
-            <ul className="hidden md:flex gap-x-7 text-navtitlecolor  display: flex  font-JioTypeBold">
+            <ul className=" md:flex gap-x-7 text-navtitlecolor  display: flex  font-JioTypeBold">
             <section>
             <span>
            <Link href="/">
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
-import Script from 'next/script'
-import Head from 'next/head'
-import Card from './components/Card'
+
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import SlideShow from './components/Carousel'
@@ -9,24 +7,31 @@ import Cards from './components/Cards'
 import FooterDivider from './components/FooterDivider'
 import Resource from './components/Resources'
 import AboutJio from './components/AboutJio'
+import Script from "next/script";
+
+//vids
+
 
 export default function Home() {
   return (
     <>
       <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-carousel.js" />
-      <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-video-grid.js" />
       <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-storyblock.js" />
-
+      <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-video-grid.js" />
       <div>
         <Navbar />
         <SlideShow />
         <Cards />
         <Divider />
+        <je-storyblock playlist-id="IXF-104" tenant-id="649ed93966cbc10030401517"  user-id="dummy_id"></je-storyblock>
         <AboutJio />
+        <je-carousel playlist-id="IXF-104" tenant-id="649ed93966cbc10030401517" tags={false} user-id="dummy_id"></je-carousel>
         <Resource />
         <FooterDivider />
+        <je-video-grid playlist-id="IXF-104" tenant-id="649ed93966cbc10030401517" tags={false}  user-id="dummy_id"></je-video-grid>
         <Footer />
       </div>
     </>
+
   )
 }
