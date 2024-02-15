e84835254b90d6adee437e16b75341da50090cac
nehallsharma <sharmanehal347@gmail.com>
2024-02-15 13:35:01 +0530

    alignment fixed aboutjio

--- src/app/components/AboutJio.js
+++ src/app/components/AboutJio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
- 
- 
+
+
 const AboutJio = () => {
     return (
         <><div className="flex flex-col items-center">
@@ -12,25 +12,26 @@ const AboutJio = () => {
             <p className='text-gray-700 text-center font-JioTypeMedium text-l'>rapidly evolving financial needs of India.</p>
             <button class="my-10 px-4 py-4 font-JioTypeBold text-black bg-navbgcolor  hover:bg-yellow-700 rounded-full">Learn more about us</button>
         </div>
-            <div class="flex justify-center items-center mb-20" >
-                <div class="grid grid-rows-2 grid-flow-col items-center gap-3 w-full md:w-3/4 lg:w-1/2">
-                    <div class="row-span-3 rounded-3xl ">
-                        <Image src="/assets/aboutjio/about-jio-finance-desk1.webp" className='rounded-3xl' alt='image' width={400} height={400} />
+            <div className="flex justify-center items-center mb-20">
+                <div className="grid grid-rows-2 grid-flow-col items-center gap-3">
+                    <div className="row-span-3 rounded-3xl w-full h-full flex justify-end">
+                        <Image src="/assets/aboutjio/about-jio-finance-desk1.webp" className='rounded-3xl' alt='image' width={200} height={450} />
                     </div>
-                    <div class="col-span-2">
-                        <Image src="/assets/aboutjio/about-jio-finance-desk2.webp"  className='rounded-3xl'alt='image' width={295} height={200} />
+                    <div className="col-span-2 rounded-3xl w-48 h-40">
+                        <Image src="/assets/aboutjio/about-jio-finance-desk2.webp" className='rounded-3xl' alt='image' width={295} height={100} />
                     </div>
-                    <div class="row-span-2 col-span-2">
+                    <div className="col-span-2 rounded-3xl w-48 h-40">
                         <Image src="/assets/aboutjio/about-jio-finance-desk3.webp" className='rounded-3xl' alt='image' width={295} height={200} />
                     </div>
-                    <div class="row-span-3">
-                        <Image src="/assets/aboutjio/about-jio-finance-desk4.webp" className='rounded-3xl' alt='image' width={400} height={400} />
+                    <div className="row-span-3 rounded-3xl w-full h-full flex justify-start">
+                        <Image src="/assets/aboutjio/about-jio-finance-desk4.webp" className='rounded-3xl' alt='image' width={200} height={450} />
                     </div>
                 </div>
             </div>
- 
+
+
         </>
     );
 };
- 
+
 export default AboutJio;
