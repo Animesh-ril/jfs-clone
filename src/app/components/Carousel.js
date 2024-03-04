b3a7c9fa2d9fe4324bb5502bf2aaad7c3f0c2930
“nehal.sharma” <nehal.sharma@ril.com>
2024-03-04 12:13:48 +0530

    Updated Alignment

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -1,89 +1,102 @@
-'use client';
+"use client";
 import React, { use, useEffect } from "react";
-import Image from 'next/image'
-import { RxDotFilled } from 'react-icons/rx'
+import Image from "next/image";
+import { RxDotFilled } from "react-icons/rx";
 import { useState } from "react";
 // import image1 from "../assets/slideshow/homepage-pay-desk.webp"
 // import Carousel from "react-multi-carousel";
 // import "react-multi-carousel/lib/styles.css";
- 
- 
- 
-const slides = [
 
+const slides = [
   {
     url: "/assets/slideshow/homepage-pay-desk.jpg",
-    plain: 'Manage',
-    coloured: ' your finances'
+    plain: "Manage",
+    coloured: " your finances",
   },
   {
-    url: "/assets/slideshow/hero-insurance-mobi.webp",
-    plain: 'Protect',
-    coloured: ' all that matters'
+    url: "/assets/slideshow/SlideShow1.png",
+    // url: "/assets/slideshow/hero-insurance-mobi.jpg",
+    // plain: "Protect",
+    // coloured: " all that matters",
   },
   {
-    url: "/assets/slideshow/hero-digital-banking-mobi.webp",
-    plain: 'Bank',
-    coloured: ' anytime anywhere'
+    url: "/assets/slideshow/SlideShow2.png",
+    // url: "/assets/slideshow/hero-digital-banking-mobi.jpg",
+    // plain: "Bank",
+    // coloured: " anytime anywhere",
   },
   {
-    url: "/assets/slideshow/hero-instant-loan-mobi.webp",
-    plain: 'Realise',
-    coloured: ' your dreams'
+    url: "/assets/slideshow/SlideShow3.png",
+    // url: "/assets/slideshow/hero-instant-loan-mobi.jpg",
+    // plain: "Realise",
+    // coloured: " your dreams",
   },
-  
-]
- 
- 
+];
+
 export default function SlideShow() {
- 
- 
- 
-  const [currentIndex, setCurrentIndex] = useState(0)
- 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const prevSlide = () => {
     const isFirst = currentIndex === 0;
-    const newIndex = isFirst ? slides.length - 1 : currentIndex - 1
-    setCurrentIndex(newIndex)
-  }
- 
+    const newIndex = isFirst ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
   const nextSlide = () => {
     const isLast = currentIndex === slides.length - 1;
     const newIndex = isLast ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex)
-  }
- 
+    setCurrentIndex(newIndex);
+  };
+
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
-  }
- 
- 
+  };
+
   return (
     <div className="max-w-[1900px] h-[650px] w-full m-auto  relative ">
- 
       <div>
         {/* <Image src={"/assets/slideshow/homepage-pay-desk.webp"} width={1900} height={650} className="object-cover w-full h-[680px]"/> */}
       </div>
- 
-      <div style={{ backgroundImage: `url(${slides[currentIndex].url})`, backgroundSize:"cover", backgroundRepeat:"no-repeat", transition: 'background-image 0.5s ease' }} className="w-full h-full bg-center bg-cover duration-500 transition ease-in-out">
-        <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center md:block">
-          <div className="mb-10 text-5xl ">
-            <h5 className=" text-white font-JioTypeBlack">{slides[currentIndex].plain}
-              <span className="text-navbgcolor font-JioTypeBlack">{slides[currentIndex].coloured}</span></h5>
+
+      <div
+        style={{
+          backgroundImage: `url(${slides[currentIndex].url})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          transition: "background-image 0.5s ease",
+        }}
+        className="w-full h-full bg-center bg-cover duration-500 transition ease-in-out"
+      >
+        {slides[currentIndex]?.plain && (
+          <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center md:block">
+            <div className="mb-10 text-5xl ">
+              <h5 className=" text-white font-JioTypeBlack">
+                {slides[currentIndex].plain}
+                <span className="text-navbgcolor font-JioTypeBlack">
+                  {slides[currentIndex].coloured}
+                </span>
+              </h5>
+            </div>
+            <button class="px-4 py-4 font-JioTypeBold text-carouseltext bg-navbgcolor  hover:bg-navtitlecolor rounded-full">
+              Explore Now
+            </button>
           </div>
-          <button class="px-4 py-4 font-JioTypeBold text-carouseltext bg-navbgcolor  hover:bg-navtitlecolor rounded-full">Explore Now</button>
-        </div>
+        )}
       </div>
-      <div className="flex top-4 justify-center py-2">
+      <div className="flex top-4 justify-center py-4 gap-x-3">
         {slides.map((slide, slideIndex) => (
-          <div key={slideIndex} onClick={() => goToSlide(slideIndex)} className="text-2xl cursor-pointer">
-            <RxDotFilled  className=" text-stone-400 "/>
-          </div>
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className={`${
+              slideIndex === currentIndex
+                ? "bg-[#FAEBD9] min-w-6"
+                : "bg-[#b5b5b5] min-w-2"
+            } transition-all min-h-2 rounded-full cursor-pointer`}
+          ></div>
         ))}
       </div>
-      <div>
- 
-      </div>
+      <div></div>
     </div>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,69 @@
-
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import SlideShow from './components/Carousel'
-import Divider from './components/Divider'
-import Cards from './components/Cards'
-import FooterDivider from './components/FooterDivider'
-import Resource from './components/Resources'
-import AboutJio from './components/AboutJio'
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import SlideShow from "./components/Carousel";
+import Divider from "./components/Divider";
+import Cards from "./components/Cards";
+import FooterDivider from "./components/FooterDivider";
+import Resource from "./components/Resources";
+import AboutJio from "./components/AboutJio";
 import Script from "next/script";
 
 //vids
 
- 
 export default function Home() {
   return (
     <>
-      <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-carousel.js" />
-      <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-storyblock.js" />
-      <Script type="module" src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-video-grid.js" />
+      <Script
+        type="module"
+        src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-carousel.js"
+      />
+      <Script
+        type="module"
+        src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-storyblock.js"
+      />
+      <Script
+        type="module"
+        src="https://jioplayalong4.akamaized.net/jiostream-vcom-prod/dist/je-video-grid.js"
+      />
       <div>
         <Navbar />
         <SlideShow />
-        <Cards />
-        <div className='bg-gray-100 p-10'>
-        <h1 className='font-JioTypeBlack text-navtitlecolor -mt-7 mb-10 text-5xl text-center'>Learn about Finance & Investments</h1>
-        <je-carousel playlist-id="IXF-104" tenant-id="649ed93966cbc10030401517" tags={false} user-id="dummy_id"></je-carousel>
+        <div className="bg-gray-100 p-10">
+          <h1 className="font-JioTypeBlack text-navtitlecolor mt-7 mb-10 text-5xl text-center">
+            Learn about Finance & Investments
+          </h1>
+          <je-carousel
+            playlist-id="IXF-104"
+            tenant-id="649ed93966cbc10030401517"
+            tags={false}
+            user-id="dummy_id"
+          ></je-carousel>
         </div>
+        <Cards />
         <Divider />
         <AboutJio />
         <Resource />
-        <div className='p-10 bg-resourcebg'>
-
-        <je-storyblock playlist-id="IXF-104" tenant-id="649ed93966cbc10030401517"  user-id="dummy_id"></je-storyblock>
+        <div className="p-10 bg-resourcebg">
+          <je-storyblock
+            playlist-id="IXF-104"
+            tenant-id="649ed93966cbc10030401517"
+            user-id="dummy_id"
+          ></je-storyblock>
         </div>
         <FooterDivider />
-        <div className=' p-10'>
-        <h1 className='font-JioTypeBlack text-navtitlecolor -mt-10 mb-10 text-5xl text-center'>Lets learn to Invest</h1>
-        <je-video-grid playlist-id="IXF-104" tenant-id="649ed93966cbc10030401517" tags={false}  user-id="dummy_id"></je-video-grid>
+        <div className=" p-10">
+          <h1 className="font-JioTypeBlack text-navtitlecolor -mt-10 mb-10 text-5xl text-center">
+            Lets learn to Invest
+          </h1>
+          <je-video-grid
+            playlist-id="IXF-104"
+            tenant-id="649ed93966cbc10030401517"
+            tags={false}
+            user-id="dummy_id"
+          ></je-video-grid>
         </div>
         <Footer />
       </div>
     </>
-  )
+  );
 }
